e4cf1a35815687e8f4ea7782d6c49973dd949b96
Dütschler, Benjamin (STUDENTS) <benjamin.duetschler1@students.unibe.ch>
2023-11-08 13:45:53 +0100

    update shader for earth and set various uniforms

--- src/solar_viewer.cpp
+++ src/solar_viewer.cpp
@@ -416,12 +416,21 @@ void Solar_viewer::draw_scene(mat4& _projection, mat4& _view)
     m_matrix = mat4::translate(earth_.pos_) * mat4::rotate_y(earth_.angle_self_) * mat4::scale(earth_.radius_);
     mv_matrix = _view * m_matrix;
     mvp_matrix = _projection * mv_matrix;
-    color_shader_.use();
-    color_shader_.set_uniform("modelview_projection_matrix", mvp_matrix);
-    color_shader_.set_uniform("t", earth_animation_time, true);
-    color_shader_.set_uniform("tex", 0);
-    color_shader_.set_uniform("greyscale", (int)greyscale_);
+    earth_shader_.use();
+    earth_shader_.set_uniform("light_position", light);
+    earth_shader_.set_uniform("normal_matrix", _view * inverse(transpose(mat3(m_matrix))));
+    earth_shader_.set_uniform("modelview_matrix", mv_matrix);
+    earth_shader_.set_uniform("modelview_projection_matrix", mvp_matrix);
+    earth_shader_.set_uniform("t", earth_animation_time, true);
+    earth_shader_.set_uniform("day_texture", 0);
+    earth_shader_.set_uniform("night_texture", 1);
+    earth_shader_.set_uniform("cloud_texture", 2);
+    earth_shader_.set_uniform("gloss_texture", 3);
+    earth_shader_.set_uniform("greyscale", (int)greyscale_);
     earth_.tex_.bind();
+    earth_.night_.bind();
+    earth_.cloud_.bind();
+    earth_.gloss_.bind();
     unit_sphere_.draw();
 
 
